7330d5f683c70e321f3dfa041dfa5daa6b99d64e
Harshil Lodhi <harslo@microsoft.com>
2015-12-18 14:06:08 +0530

    Shell++ completed

--- Utilites/InlineShell/task.js
+++ Utilites/InlineShell/task.js
@@ -1,4 +1,3 @@
-
 var path = require('path');
 var tl = require('vso-task-lib');
 var fs = require('fs');
@@ -7,29 +6,27 @@ var echo = new tl.ToolRunner(tl.which('echo', true));
 var bash = new tl.ToolRunner(tl.which('bash', true));
 
 var cwd = tl.getPathInput('cwd', false);
-var script = tl.getInput('script', true);
+var script = tl.getInput('script', false);
+var failOnStdErr = tl.getInput('failOnStandardError') == 'true';
+var scriptPath = tl.getPathInput('scriptPath', false);
+var type = tl.getInput('type');
 
-
-// will error and fail task if it doesn't exist
-if (cwd == null || cwd == "") {
-	cwd = "/tmp";
-}
-
-tl.debug('using cwd: ' + cwd);
-tl.cd(cwd);
-
-var scriptPath = cwd + "/user_script.sh"; 
-fs.writeFile(scriptPath, script, function(err) {
-    if(err) {
-        console.error(err.message);
-		tl.debug('user script file creation failed.');
-		tl.exit(1);
-		return;
-    }
-
-    bash.arg(scriptPath);
+if(!type){
+	// nothing to do
+	tl.warning("type is not mentioned")
+}else if(type == 'InlineScript'){
+	if (cwd == null || cwd == "") {
+		cwd = "/tmp";
+	}
+			
+	tl.debug('using cwd: ' + cwd);
+	tl.cd(cwd);
 	
-    bash.exec({ failOnStdErr: true})
+	scriptPath = cwd + "/user_script.sh"; 
+	fs.writeFileSync(scriptPath,script,'utf8');
+	
+	bash.arg(scriptPath);
+	bash.exec({ failOnStdErr: failOnStdErr})
 	.then(function(code) {
 		// TODO: switch to setResult in the next couple of sprints
 		tl.exit(code);
@@ -39,8 +36,29 @@ fs.writeFile(scriptPath, script, function(err) {
 		tl.debug('taskRunner fail');
 		tl.exit(1);
 	});
-
-});
-
-
-
+}else if(type == 'FilePath'){
+	if(!scriptPath){
+		tl.warning("No script to execute");
+		tl.exit(1);
+	}
+	if (!cwd) {
+		cwd = path.dirname(scriptPath);
+	}
+	tl.debug('using cwd: ' + cwd);
+	tl.cd(cwd);
+	bash.arg(scriptPath);
+	bash.arg(tl.getInput('args', false));
+	bash.exec({ failOnStdErr: failOnStdErr})
+	.then(function(code) {
+		// TODO: switch to setResult in the next couple of sprints
+		tl.exit(code);
+	})
+	.fail(function(err) {
+		console.error(err.message);
+		tl.debug('taskRunner fail');
+		tl.exit(1);
+	});
+}else{
+	tl.debug("something is wrong buddy");
+	tl.exit(1);
+}
