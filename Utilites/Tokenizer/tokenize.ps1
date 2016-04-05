5594db8a5fdbe9fb105476988f31751487d60833
Pascal Berger <pascal.berger@ioz.ch>
2016-04-05 15:16:46 +0200

    Add logging messages

--- Utilites/Tokenizer/tokenize.ps1
+++ Utilites/Tokenizer/tokenize.ps1
@@ -9,6 +9,11 @@ param
     [String] [Parameter(Mandatory = $false)] $ConfigurationJsonFile
 )
 
+Write-Verbose "Entering script tokenize.ps1"
+Write-Verbose "SourcePath = $SourcePath"
+Write-Verbose "DestinationPath = $DestinationPath"
+Write-Verbose "ConfigurationJsonFile = $ConfigurationJsonFile"
+
 . $PSScriptRoot\Helpers.ps1
 
 #ConfigurationJsonFile has multiple environment sections.
@@ -18,12 +23,14 @@ if (Test-Path -Path env:RELEASE_ENVIRONMENTNAME){
 }
 Write-Host "Environment:"$environmentName
 # Validate that $SourcePath is a valid path
+Write-Verbose "Validate that SourcePath is a valid path: $SourcePath"
 if (!(Test-Path -Path $SourcePath)){
     throw "$SourcePath is not a valid path. Please provide a valid path"
 }
 
 # Set $DestinationPath as $SourcePath if it is not passed as input. So, SourceFile gets transformed as DestinationFile
 if($DestinationPath -eq ''){
+    Write-Verbose "No DestinationPath passed. Use '$SourcePath' as DestinationPath"
     $DestinationPath = $SourcePath
 }
 
@@ -31,6 +38,7 @@ if($DestinationPath -eq ''){
 $SourceIsXml=Test-ValidXmlFile $SourcePath
 # Is there a valid Configuration Json input provided for modifying configuration
 if($ConfigurationJsonFile -ne ''){
+    Write-Verbose "Using configuration from '$ConfigurationJsonFile'"
     $Configuration=Get-JsonFromFile $ConfigurationJsonFile
 } 
 
@@ -40,6 +48,8 @@ if($ConfigurationJsonFile -ne ''){
 #>
 
 if(($SourceIsXml) -and ($Configuration)){
+    Write-Verbose "'$SourcePath' is a XML file. Apply all configurations from '$ConfigurationJsonFile'"
+
     $keys= $Configuration.$environmentName.ConfigChanges
 
     $xmlraw=[xml](Get-Content $SourcePath)
@@ -51,6 +61,7 @@ if(($SourceIsXml) -and ($Configuration)){
                 $node.($key.Attribute)=$key.Value
                 }
             catch{
+                Write-Error "Failure while updating " $key.Attribute "of " $key.KeyName ":" $key.Value
             }
         }
     }
@@ -72,6 +83,7 @@ Copy-Item -Force $DestinationPath $tempFile
 $matches = select-string -Path $tempFile -Pattern $regex -AllMatches | % { $_.Matches } | % { $_.Value }
 ForEach($match in $matches)
 {
+  Write-Host "Updating token '" $match "'" 
   $matchedItem = $match
   $matchedItem = $matchedItem.Trim('_')
   $matchedItem = $matchedItem -replace '\.','_'
@@ -81,15 +93,21 @@ ForEach($match in $matches)
     try{
         if(Test-Path env:$matchedItem){
             $variableValue=(get-item env:$matchedItem).Value
+            Write-Verbose "Found custom variable '$matchedItem' in build or release definition with value '$variableValue'" 
             }
         else{
             if($Configuration.$environmentName.CustomVariables.$matchedItem){
                 $variableValue=$Configuration.$environmentName.CustomVariables.$matchedItem
+                Write-Verbose "Found variable '$matchedItem' in configuration with value '$variableValue" 
+            }
+            else {
+                Write-Host "No value found for token '" $match "'"
             }
         }
         }
     catch{
         $variableValue=$match
+        Write-Host "Error searching for variable for token '" $match "'"
     }
     $_ -replace $match,$variableValue
   } | 
