923dea598104d59a526df92c377a912a15191d26
harshil93 <lodhi.harshil@gmail.com>
2016-04-30 21:19:54 +0530

    Tokenizer tasks ported to use the new handler and sdk.

---
+++ Utilites/Tokenizer/tokenize-ps3.ps1
@@ -0,0 +1,119 @@
+[CmdletBinding()]
+param()
+
+Trace-VstsEnteringInvocation $MyInvocation
+
+try {
+    [string]$SourcePath = Get-VstsInput -Name SourcePath
+    [string]$DestinationPath = Get-VstsInput -Name DestinationPath
+    [string]$ConfigurationJsonFile = Get-VstsInput -Name ConfigurationJsonFile
+    . $PSScriptRoot\Helpers.ps1
+    Write-Verbose "Entering script tokenize.ps1"
+    Write-Verbose "SourcePath = $SourcePath"
+    Write-Verbose "DestinationPath = $DestinationPath"
+    Write-Verbose "ConfigurationJsonFile = $ConfigurationJsonFile"
+    
+    #ConfigurationJsonFile has multiple environment sections.
+    $environmentName = "default"
+    if (Test-Path -Path env:RELEASE_ENVIRONMENTNAME) {
+        $environmentName = (get-item env:RELEASE_ENVIRONMENTNAME).value
+    }
+    Write-Host "Environment: $environmentName"
+
+    # Validate that $SourcePath is a valid path
+    Write-Verbose "Validate that SourcePath is a valid path: $SourcePath"
+    if (!(Test-Path -Path $SourcePath)) {
+        throw "$SourcePath is not a valid path. Please provide a valid path"
+    }
+
+    # Set $DestinationPath as $SourcePath if it is not passed as input. So, SourceFile gets transformed as DestinationFile
+    if ($DestinationPath -eq "") {
+        Write-Verbose "No DestinationPath passed. Use '$SourcePath' as DestinationPath"
+        $DestinationPath = $SourcePath
+    }
+
+    # Is SourceFile an XML document
+    $SourceIsXml=Test-ValidXmlFile $SourcePath
+    # Is there a valid Configuration Json input provided for modifying configuration
+    if ($ConfigurationJsonFile -ne "") {
+        Write-Verbose "Using configuration from '$ConfigurationJsonFile'"
+        $Configuration = Get-JsonFromFile $ConfigurationJsonFile
+    } 
+
+    # Create a copy of the source file and manipulate it
+    $tempFile = $DestinationPath + '.tmp'
+    Copy-Item -Force $SourcePath $tempFile -Verbose
+
+    <#
+        Step 1:- if the SourceIsXml and a valid configuration file is provided then 
+            Run through all the XPaths in the Json Configuration and update the XML file
+    #>
+    if (($SourceIsXml) -and ($Configuration)) {
+        Write-Verbose "'$SourcePath' is a XML file. Apply all configurations from '$ConfigurationJsonFile'"
+
+        $keys = $Configuration.$environmentName.ConfigChanges
+
+        $xmlraw = [xml](Get-Content $SourcePath)
+        ForEach ($key in $keys) {
+            $node = $xmlraw.SelectSingleNode($key.KeyName)
+            if ($node) {
+                try {
+                    Write-Host "Updating $($key.Attribute) of $($key.KeyName): $($key.Value)"
+                    $node.($key.Attribute) = $key.Value
+                }
+                catch {
+                    Write-Error "Failure while updating $($key.Attribute) of $($key.KeyName): $($key.Value)"
+                }
+            }
+        }
+        $xmlraw.Save($tempFile)
+    }
+
+    <#
+    Step 2:- For each token in the source configuration that matches with the regular expression __<tokenname>__
+                i.	If there is a custom variable at build or release definition then replace the token with the value of the same..
+                ii.	If the variable is available in the configuration section of json document then replace the token with the vaule from json document
+                iii.Or else it ignores the token
+    #>
+    $regex = '__[A-Za-z0-9._-]*__'
+    $matches = select-string -Path $tempFile -Pattern $regex -AllMatches | % { $_.Matches } | % { $_.Value }
+    ForEach ($match in $matches) {
+    Write-Host "Updating token '$match'" 
+    $matchedItem = $match
+    $matchedItem = $matchedItem.Trim('_')
+    $matchedItem = $matchedItem -replace '\.','_'
+    
+    $variableValue = $match
+    try {
+        if (Test-Path env:$matchedItem) {
+            $variableValue = (get-item env:$matchedItem).Value
+            Write-Verbose "Found custom variable '$matchedItem' in build or release definition with value '$variableValue'" 
+        }
+        else {
+            if ($Configuration.$environmentName.CustomVariables.$matchedItem) {
+                $variableValue = $Configuration.$environmentName.CustomVariables.$matchedItem
+                Write-Verbose "Found variable '$matchedItem' in configuration with value '$variableValue" 
+            }
+            else {
+                Write-Host "No value found for token '$match'"              
+            }
+        }
+    }
+    catch {
+        Write-Host "Error searching for variable for token '$match'"
+    }
+    
+    (Get-Content $tempFile) | 
+        Foreach-Object {
+            $_ -replace $match, $variableValue
+        } |
+        Set-Content $tempFile -Force
+    }
+
+    Copy-Item -Force $tempFile $DestinationPath
+    Remove-Item -Force $tempFile
+    
+} finally {
+    Trace-VstsLeavingInvocation $MyInvocation
+}
+
