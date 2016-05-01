115977675b4d589da802ca25fb957468f6d47734
harshil93 <lodhi.harshil@gmail.com>
2016-05-01 23:41:15 +0530

    Added tests for environment specific json transformation

--- Tests/Tokenizer/tokenizer-ps3.tests.ps1
+++ Tests/Tokenizer/tokenizer-ps3.tests.ps1
@@ -1,10 +1,10 @@
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path | Split-Path | Split-Path
 $scriptPath = Join-Path $root "\Utilites\Tokenizer\tokenize-ps3.ps1"
 
-Import-Module "..\..\Utilites\Tokenizer\ps_modules\VstsTaskSdk" -ArgumentList @{ NonInteractive = $true }
+Import-Module (Join-Path $root "\Utilites\Tokenizer\ps_modules\VstsTaskSdk") -ArgumentList @{ NonInteractive = $true }
 
-Describe "Replace task variables" {
-    It "replaces multiple variables"{
+Describe "Replace token variables" {
+    It "replaces multiple variables defined as env variables(configuration variables)"{
         
         $env:INPUT_SOURCEPATH = $srcPath = Join-Path $env:TEMP 'source.txt'
         $env:INPUT_DESTINATIONPATH = $destPath = Join-Path $env:TEMP 'dest.txt'
@@ -21,6 +21,42 @@ Describe "Replace task variables" {
         finally {
             Remove-Item -Path $srcPath
             Remove-Item -Path $destPath
+        }
+    }
+}
+
+
+Describe "Replace token variables" {
+    It "replaces variables defined in json"{
+        
+        $env:INPUT_SOURCEPATH = $srcPath = Join-Path $env:TEMP 'source.txt'
+        $env:INPUT_DESTINATIONPATH = $destPath = Join-Path $env:TEMP 'dest.txt'
+        $env:INPUT_CONFIGURATIONJSONFILE = $jsonConfigPath = Join-Path $env:TEMP 'config.json'
+        $env:RELEASE_ENVIRONMENTNAME = 'Test'
+        $foo1val = 'I am foo1'
+        $bar1val = 'I am bar1'
+        $jsonConfigContent = @{
+            Test=@{
+                CustomVariables = @{
+                    foo1 = $foo1val
+                    bar1 = $bar1val
+                }
+            }
+        } | ConvertTo-Json
+        
+        $sourceContent = '__foo1__ __bar1__'
+        $expectedDestinationContent = $foo1val + " " + $bar1val
+                
+        try {
+            Set-Content -Value $sourceContent -Path $srcPath
+            Set-Content -Value $jsonConfigContent -Path $jsonConfigPath
+            Invoke-VstsTaskScript -ScriptBlock { . $scriptPath } 
+            Get-Content -Path $destPath | Should Be $expectedDestinationContent    
+        }
+        finally {
+            Remove-Item -Path $srcPath
+            Remove-Item -Path $destPath
+            Remove-Item -Path $jsonConfigPath
         }
     }
 }
