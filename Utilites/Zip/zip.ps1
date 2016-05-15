b44686aa148bff7a4bc6494cf233fc28ebf21e83
harshil93 <lodhi.harshil@gmail.com>
2016-05-15 21:19:32 +0530

    Zip file overwrite feature added

--- Utilites/Zip/zip.ps1
+++ Utilites/Zip/zip.ps1
@@ -1,13 +1,20 @@
 param (
     [string]$pathToZipFolder,
-    [string]$pathToZipFile
+    [string]$pathToZipFile,
+    [Boolean]$overwrite
 )
 
 Write-Verbose 'Entering sample.ps1'
 Write-Verbose "pathToZipFolder = $pathToZipFolder"
 Write-Verbose "pathToZipFile = $pathToZipFile"
-
+Write-Verbose "overwrite = $overwrite"
 # Import the Task.Common dll that has all the cmdlets we need for Build
 # import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 Add-Type -A System.IO.Compression.FileSystem
+
+if ($overwrite -and (Test-Path $pathToZipFile)){
+    Write-Verbose "Removing the old file"
+    Remove-Item $pathToZipFile
+}
+
 [IO.Compression.ZipFile]::CreateFromDirectory($pathToZipFolder, $pathToZipFile)
